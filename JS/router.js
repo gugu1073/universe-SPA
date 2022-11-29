6da29ba682885ebd4a38501a3c5bebcfe51c67ca
Gustavo <ga2157724@gmail.com>
2022-11-28 22:07:06 -0300

    remove comments

--- JS/router.js
+++ JS/router.js
@@ -20,14 +20,12 @@
     const { pathname } = window.location
     const route = this.routes[pathname]
     
-    //fetch(vai buscar) // then(quando concluir) // depois, prometo que eu vou executar uma função
-    //assicrono só começa quando o atual é concluido  
     fetch(route)
     .then(data => data.text())
     .then(html => {
      document.querySelector('#app').innerHTML = html
     })
     
-    //sicrono é quando rola uma sequencia, como se fosse uma fila
+
   }
 }
